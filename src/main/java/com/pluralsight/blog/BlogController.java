88d6d23df9a859cf23c1078c6c989e791106db1a
shlomysheps <shlomy@zebra-med.com>
2022-02-24 13:41:05 +0200

    task 1

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BlogController {
     @RequestMapping("/")
-    public String listPosts(){
-        ModelMap modelMap = new ModelMap();
+    public String listPosts(ModelMap modelMap){
         modelMap.put("title","Blog Post 1");
         return "home";
     }
